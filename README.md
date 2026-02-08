6d782b8b1612a1e52ea6f43123750a02f9c05c6a
juanse217 <juansebas08004@gmail.com>
2026-02-08 18:37:06 -0500

    feat(service): add calculateFee method and other methods to support the main one to return the fee with all the information needed.

--- README.md
+++ README.md
@@ -11,3 +11,10 @@ Most of the best practices used here were already discussed in previous projects
 The repository doesn't have the reponsibility of assigning the entry date. It just has to Store and retrieve data. 
 
 If we had an ID, the logic to create the ID automatically (like in the previous project order-management), it should go on the constructor of the domain class, even if no-arg constructor.
+
+## BIGDECIMAL
+We use BigDecimal class in this project because it helps avoiding rounding errors that can happen with double or other numeric values. Usually used for money related processes or when we need precision. 
+
+## CALCULATEFEE METHOD (SERVICE)
+
+You can see in the service we have 2 methods. One is used for "orchestration", if we can call it that; we focus on getting the car, getting the fee and returning the DTO; these are too many responsibilities for a single method, if we want to change the logic, we'd need to change that whole method. By dividing it, we make that the ```calculateFeeAmount``` only has the purpose of returning the total fee and our other method only focuses on retreving information and returning. 
--- src/main/java/com/sebastian/parkinglot/service/ParkingService.java
+++ src/main/java/com/sebastian/parkinglot/service/ParkingService.java
@@ -1,11 +1,15 @@
 package com.sebastian.parkinglot.service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.Duration;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.sebastian.parkinglot.dto.CarDTO;
 import com.sebastian.parkinglot.dto.CreateDTO;
+import com.sebastian.parkinglot.dto.FeeResponseDTO;
 import com.sebastian.parkinglot.exceptions.CarAlreadyExistsException;
 import com.sebastian.parkinglot.exceptions.CarNotFoundException;
 import com.sebastian.parkinglot.exceptions.InvalidLicensePlateException;
@@ -14,7 +18,7 @@ import com.sebastian.parkinglot.repository.ParkingRepository;
 
 @Service
 public class ParkingService {
-
+    private static final BigDecimal MINUTE_RATE = new BigDecimal("0.9");
     private final ParkingRepository repo; 
 
     public ParkingService(ParkingRepository repo){
@@ -30,7 +34,9 @@ public class ParkingService {
     public CarDTO addCar(CreateDTO dto){
         validatePlate(dto.getPlate());
 
-        if(repo.findByPlate(dto.getPlate()).isPresent()) throw new CarAlreadyExistsException("The car" + dto.getPlate() + " is already parked"); //not using the findByPlate method since it throws an exception if the car doesn't exist. 
+        if(repo.findByPlate(dto.getPlate()).isPresent()){ 
+            throw new CarAlreadyExistsException("The car " + dto.getPlate() + " is already parked");
+        } //not using the findByPlate method since it throws an exception if the car doesn't exist. 
 
         Car car = new Car(dto.getPlate());
         repo.save(car);
@@ -38,10 +44,14 @@ public class ParkingService {
         return new CarDTO(car);
     }
 
+    private Car findCarOrThrow(String plate){
+        return repo.findByPlate(plate).orElseThrow(() -> new CarNotFoundException("Car with plate " + plate + " not found"));
+    }
+
     public CarDTO findByPlate(String plate){
         validatePlate(plate);
 
-        Car car = repo.findByPlate(plate).orElseThrow(() -> new CarNotFoundException("Car with plate " + plate + " not found"));
+        Car car = findCarOrThrow(plate);
 
         return new CarDTO(car);
     }
@@ -52,10 +62,28 @@ public class ParkingService {
 
     public void deleteCar(String plate){
         validatePlate(plate);
-        //findByPlate(plate); //Just using it for validating existence. 
-
-        repo.findByPlate(plate).orElseThrow(() -> new CarNotFoundException("Car with plate " + plate + " not found")); //better approach. Not creating DTOs that are unnecesary and more explicit. 
+        findCarOrThrow(plate); 
 
         repo.deleteCar(plate);
+    }
+
+    public FeeResponseDTO calculateFee(String plate){
+        validatePlate(plate);
+
+        Car car = findCarOrThrow(plate);
+        BigDecimal fee = calculateFeeAmount(car.getTimeIn());
+
+        return new FeeResponseDTO(plate, formatDuration(car.getTimeIn()), fee);
+    }
+
+    private BigDecimal calculateFeeAmount(Duration timeIn){
+        BigDecimal timeInMins = new BigDecimal(timeIn.toMinutes());
+        return MINUTE_RATE.multiply(timeInMins).setScale(2, RoundingMode.HALF_UP);//specifies how many decimal places and how to round. We can have only the decimal places
+    }
+
+    private String formatDuration(Duration duration){
+        long hrs = duration.toHours();
+        int mins = duration.toMinutesPart();
+        return String.format("\\dh \\dm", hrs, mins);
     }
 }
